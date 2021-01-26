f3cd59c543b3527a9da0291b5383f49b3d51a8e4
mam1 <mam1@cornell.edu>
2021-01-26 10:01:29 -0500

    playing

--- tests/load_channels/load_channels.c
+++ tests/load_channels/load_channels.c
@@ -1,5 +1,5 @@
 /* 
-    test of a sql based scheduler
+    load channel data
 */
 
 #include "mysql.h"
@@ -30,43 +30,36 @@ static void show_stmt_error(MYSQL_STMT *stmt)
 }
 
 void load_channels(void){
-  
-}
-
-int main(int argc, char *argv[])
-{
   MYSQL *mysql;
   MYSQL_STMT *stmt;
   MYSQL_BIND bind[3];
 
   /* Data for insert */
-  const char *surnames[]= {"Widenius", "Axmark", "N.N."};
-  unsigned long surnames_length[]= {8,6,4};
-  const char *forenames[]= {"Monty", "David", "will be replaced by default value"};
-  char forename_ind[]= {STMT_INDICATOR_NTS, STMT_INDICATOR_NTS, STMT_INDICATOR_DEFAULT};
+  const char *name[]= {"test channel 1", "test channel 2", "test channel 3"};
+  unsigned long name_length[]= {14,14,14};
+  const bool *active[]= {0,0,0}
+
+  char name_ind[]= {STMT_INDICATOR_NTS, STMT_INDICATOR_NTS, STMT_INDICATOR_DEFAULT}; 
   char id_ind[]= {STMT_INDICATOR_NULL, STMT_INDICATOR_NULL, STMT_INDICATOR_NULL};
+
   unsigned int array_size= 3; 
 
   mysql= mysql_init(NULL);
 
-  /* connect to MariaDB server */
+   /* connect to MariaDB server */
   if (!mysql_real_connect(mysql, SERVER, USER, PSWD, DATABASE, 0, SOCKETT, 0))
     show_mysql_error(mysql);
 
-  // if (mysql_query(mysql, "DROP TABLE IF EXISTS bulk_example1"))
-  //   show_mysql_error(mysql);
-
-  // if (mysql_query(mysql, "CREATE TABLE bulk_example1 (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"\
-  //                        "forename CHAR(30) NOT NULL DEFAULT 'unknown', surname CHAR(30))"))
-  //   show_mysql_error(mysql);
+  printf("connection to %s established", SERVER);
 
   stmt= mysql_stmt_init(mysql);
-  if (mysql_stmt_prepare(stmt, "INSERT INTO bulk_example1 VALUES (?,?,?)", -1))
+  if (mysql_stmt_prepare(stmt, "INSERT INTO channels VALUES (?,?,?)", -1))
     show_stmt_error(stmt);
 
   memset(bind, 0, sizeof(MYSQL_BIND) * 3);
 
-  /* We autogenerate id's, so all indicators are STMT_INDICATOR_NULL */
+
+
   bind[0].u.indicator= id_ind;
   bind[0].buffer_type= MYSQL_TYPE_LONG;
 
@@ -77,6 +70,29 @@ int main(int argc, char *argv[])
   bind[2].buffer_type= MYSQL_TYPE_STRING;
   bind[2].buffer= surnames;
   bind[2].length= &surnames_length;
+
+
+  
+
+}
+
+int main(int argc, char *argv[])
+{
+
+  printf("%s\n", "\ntest data loader\n");
+  printf("%s\n", );
+
+  printf("   SERVER   %s\n",SERVER);
+  printf("   SOCKETT  %s\n",SOCKETT);
+  printf("   DATABASE %s\n",DATABASE);
+  printf("   USER     %s\n",USER);
+  printf("   PSWD     %s\n\n",PSWD);
+  
+
+
+
+  /* We autogenerate id's, so all indicators are STMT_INDICATOR_NULL */
+
 
   /* set array size */
   mysql_stmt_attr_set(stmt, STMT_ATTR_ARRAY_SIZE, &array_size);
