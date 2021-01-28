9aa22b853142c8d4d293a6809a6378e4a1c8d7f1
mam1 <mam1@cornell.edu>
2021-01-28 11:09:38 -0500

    playing

--- tests/load_channels/load_channels.c
+++ tests/load_channels/load_channels.c
@@ -121,7 +121,7 @@ static void load_channels(void){
   printf("%s\n", "test channel data loaded\n");
 
 
-  if (mysql_query(mysql, "xxxxxxxxxxxxx"))
+  if (mysql_query(mysql, "SELECT * FROM channel"))
   {
       finish_with_error(mysql);
   }
