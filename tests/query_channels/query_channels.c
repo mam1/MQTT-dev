639bdf3820b8fbe9d9bb9b7d330d3ec61261233c
mam1 <mam1@cornell.edu>
2021-02-04 15:28:07 -0500

    working

--- tests/query_channels/query_channels.c
+++ tests/query_channels/query_channels.c
@@ -81,7 +81,7 @@ int main(int argc, char* argv[]) {
 
   result = mysql_store_result(conn);
   printf("number of rows effected %i\n", (int)mysql_num_rows(result) );
-  
+  printf("number of fields %i\n", (int)mysql_num_fields(result));
   // mysql_stmt_free_result(stmt);
   mysql_free_result(result);
   mysql_close(conn);
