e2f2d8caa53eb8136cf66112aa2cc9a74a958f6d
mam1 <mam1@cornell.edu>
2021-02-05 10:28:14 -0500

    working

--- tests/query_channels/query_channels.c
+++ tests/query_channels/query_channels.c
@@ -92,10 +92,11 @@ int main(int argc, char* argv[]) {
   for(i=0;i<(int)mysql_num_fields(result);i++){
     mysql_field_seek(result,i);
     field = mysql_fetch_field(result);
+    printf("column name <%s>\n", field->name);
   }
  
 
-  printf("column name <%s>\n", field->name);
+  
   // mysql_stmt_free_result(stmt);
   mysql_free_result(result);
   mysql_close(conn);
