4b54558e07d3d436d337e7fabf8fcbd9a9fcf334
mam1 <mam1@cornell.edu>
2021-02-08 13:33:04 -0500

    working

--- tests/time/timet.c
+++ tests/time/timet.c
@@ -138,11 +138,11 @@ int main(int argc, char* argv[]) {
 
 
 
-		// for (i = 0; i < (int)mysql_num_fields(result); i++) {
-		// 	mysql_field_seek(result, i);
-		// 	field = mysql_fetch_field(result);
-		// 	printf("column %i <%s> \t%s\n", i, field->name, row[i]);
-		// }
+		for (i = 0; i < (int)mysql_num_fields(result); i++) {
+			mysql_field_seek(result, i);
+			field = mysql_fetch_field(result);
+			printf("column %i <%s> \t%s\n", i, field->name, row[i]);
+		}
 
 
 		/********************************************************************/
