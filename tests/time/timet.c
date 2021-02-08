cd1055677074be488ec13c52c027f3ccc024f61d
mam1 <mam1@cornell.edu>
2021-02-08 09:37:34 -0500

    working

--- tests/time/timet.c
+++ tests/time/timet.c
@@ -98,14 +98,14 @@ int main(int argc, char* argv[]) {
 	}
 
 	// if (mysql_query(conn, "SELECT Channels.*,Schedules.* FROM Channels C INNER JOIN Schedules S USING(scheduleID) WHERE C.enabled = 'yes'"))
-	if (mysql_query(conn, "SELECT Channels.* FROM Channels  WHERE enabled = 'yes'"))
+	if (mysql_query(conn, "SELECT Channels.* FROM Channels  INNER JOIN Schedules ON Channels.scheduleID = Schedules.scheduleID WHERE Channel.enabled = 'yes'"))
 		show_mysql_error(conn);
 	result = mysql_store_result(conn);
 
 	while ((row = mysql_fetch_row(result)) != NULL) {
 		mysql_field_seek(result, 7);
 		field = mysql_fetch_field(result);
-		printf("channel %s \n",  row[2] );
+		printf("processing channel <%s> \n",  row[2] );
 /********************************************************************/		
 
 
