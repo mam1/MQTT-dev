bd33f50b0b30643e7c1c77f9c5f4d789ebefba71
mam1 <mam1@cornell.edu>
2021-02-10 14:22:11 -0500

    under construstion

--- tests/joins/join.c
+++ tests/joins/join.c
@@ -100,7 +100,7 @@ int main(int argc, char* argv[]) {
 
 	// if (mysql_query(conn, "SELECT Channels.*,Schedules.* FROM Channels C INNER JOIN Schedules S USING(scheduleID) WHERE C.enabled = 'yes'"))
 	// if (mysql_query(conn, "SELECT * FROM Channels  INNER JOIN Schedules USING(scheduleID) INNER JOIN Sensors USING(sensorID) INNER JOIN Transitions USING(scheduleID)"))
-	if (mysql_query(conn, "SELECT * FROM Channels INNER JOIN Schedules USING(scheduleID) WHERE enabled = 'yes'"))
+	if (mysql_query(conn, "SELECT * FROM Channels INNER JOIN Schedules USING(scheduleID) WHERE channel_enabled = 'yes'"))
 		show_mysql_error(conn);
 	result = mysql_store_result(conn);
 	printf("we now have %i  active columns\n", mysql_num_fields(result));
