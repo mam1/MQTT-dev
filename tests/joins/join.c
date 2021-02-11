9f8d95e60dd933fe558555305539d36a060573f9
mam1 <mam1@cornell.edu>
2021-02-11 15:49:55 -0500

    under construstion

--- tests/joins/join.c
+++ tests/joins/join.c
@@ -80,10 +80,10 @@ int main(int argc, char* argv[])
 		printf("couldn't connect to database\n");
 		exit(1);
 	}
-	// if (mysql_query(conn, "SELECT Channels.*, Transitions.*, Schedules.* FROM Channels JOIN Transitions USING(scheduleID) INNER JOIN Schedules USING(scheduleID) WHERE Channels.scheduleID = Transitions.scheduleID AND Transitions.transition_offset > 250 AND Transitions.transition_offset < 800")) show_mysql_error(conn);
-	// if (mysql_query(conn, "SELECT Channels.*, Transitions.*, Schedules.* FROM Channels JOIN Transitions USING(scheduleID) INNER JOIN Schedules USING(scheduleID) WHERE Channels.scheduleID = Transitions.scheduleID AND Transitions.transition_offset > 250 AND Transitions.transition_offset < 800")) show_mysql_error(conn);
+	// if (mysql_query(conn, "SELECT Channels.*, Transitions.*, Schedules.* FROM Channels JOIN Transitions USING(scheduleID) INNER JOIN Schedules USING(scheduleID) WHERE Channels.scheduleID = Transitions.scheduleID AND Transitions.transition_offset > 410 AND Transitions.transition_offset < 800")) show_mysql_error(conn);
+	// if (mysql_query(conn, "SELECT Channels.*, Transitions.*, Schedules.* FROM Channels JOIN Transitions USING(scheduleID) INNER JOIN Schedules USING(scheduleID) WHERE Channels.scheduleID = Transitions.scheduleID AND Transitions.transition_offset > 410 AND Transitions.transition_offset < 800")) show_mysql_error(conn);
 
-	if (mysql_query(conn, "SELECT Channels.*, Transitions.*, Schedules.* FROM Channels JOIN Transitions USING(scheduleID) INNER JOIN Schedules USING(scheduleID) WHERE Channels.scheduleID = Transitions.scheduleID AND Transitions.transition_offset = 250")) show_mysql_error(conn);
+	if (mysql_query(conn, "SELECT Channels.*, Transitions.*, Schedules.* FROM Channels JOIN Transitions USING(scheduleID) INNER JOIN Schedules USING(scheduleID) WHERE Channels.scheduleID = Transitions.scheduleID AND Transitions.transition_offset = 410")) show_mysql_error(conn);
 	result = mysql_store_result(conn);
 	rows_returned = (int)mysql_num_rows(result);
 	printf("%i rows returned\n", rows_returned);
@@ -102,7 +102,7 @@ int main(int argc, char* argv[])
 	else
 	{
 		mysql_free_result(result);
-		if (mysql_query(conn, "SELECT Channels.*, Transitions.*, Schedules.* FROM Channels JOIN Transitions USING(scheduleID) INNER JOIN Schedules USING(scheduleID) WHERE Channels.scheduleID = Transitions.scheduleID AND Transitions.transition_offset < 250 ")) show_mysql_error(conn);
+		if (mysql_query(conn, "SELECT Channels.*, Transitions.*, Schedules.* FROM Channels JOIN Transitions USING(scheduleID) INNER JOIN Schedules USING(scheduleID) WHERE Channels.scheduleID = Transitions.scheduleID AND Transitions.transition_offset < 410 ")) show_mysql_error(conn);
 		result = mysql_store_result(conn);
 		rows_returned = (int)mysql_num_rows(result);
 		printf("%i rows returned\n", rows_returned);
