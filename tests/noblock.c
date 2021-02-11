718cf544dc457083bf3a4d06fe19830b6c9a69e5
mam1 <mam1@cornell.edu>
2021-02-11 16:42:38 -0500

    under construstion

---
+++ tests/noblock.c
@@ -0,0 +1,2 @@
+// in modern C programs, you can simplify this another way, by putting the console I/O into a thread or lightweight process. 
+// Then the I/O can go on in the usual blocking fashion, but the data can be inserted into a queue to be processed on another thread.
