ea46fff8846c98ef75c6c4ff51a367c70a4c2e09
mam1 <mam1@cornell.edu>
2021-02-12 10:37:13 -0500

    underdevelopment

--- tests/noblock/noblock.c
+++ tests/noblock/noblock.c
@@ -1,23 +1,26 @@
-// in modern C programs, you can simplify this another way, by putting the console I/O into a thread or lightweight process. 
+// in modern C programs, you can simplify this another way, by putting the console I/O into a thread or lightweight process.
 // Then the I/O can go on in the usual blocking fashion, but the data can be inserted into a queue to be processed on another thread.
 
 
-    #include <unistd.h>
-    #include <stdio.h>
-    int main(int argc, char *argv[]) {
-      pid_t fork_pid = fork();
-      if (fork_pid == 0) {
-        printf("Hello from the child!\n");
-        int execl(const char *filename, const char *arg, ...);
-      } else {
-        printf("Hello from the parent!\n");
-      }
-      return 0;
-    }
+#include <unistd.h>
+#include <stdio.h>
 
-    // #include <unistd.h>
-    // int main(int argc, char *argv[]) {
-    //   execl("/bin/bash", "/yes/its/bash", "-c", "echo $0 && uptime", NULL);
-      
-    //   return 0;
-    // }
+
+int main(int argc, char *argv[]) {
+  const char      file[]="conwatcher"
+  pid_t fork_pid = fork();
+  if (fork_pid == 0) {
+    printf("Hello from the child!\n");
+    int execl( file,,);
+  } else {
+    printf("Hello from the parent!\n");
+  }
+  return 0;
+}
+
+// #include <unistd.h>
+// int main(int argc, char *argv[]) {
+//   execl("/bin/bash", "/yes/its/bash", "-c", "echo $0 && uptime", NULL);
+
+//   return 0;
+// }
