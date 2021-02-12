b72104499e1ebb2d566dfa2e8c30b3b18bd5f07d
mam1 <mam1@cornell.edu>
2021-02-12 10:28:12 -0500

    underdevelopment

---
+++ tests/conwatcher/conwatcher.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <stdio.h>
+
+
+#include <unistd.h>
+int main(int argc, char *argv[]) {
+	printf("%s\n", "conwatcher active");
+	return 0;
+}
--- tests/noblock/noblock.c
+++ tests/noblock/noblock.c
@@ -2,21 +2,22 @@
 // Then the I/O can go on in the usual blocking fashion, but the data can be inserted into a queue to be processed on another thread.
 
 
-    // #include <unistd.h>
-    // #include <stdio.h>
-    // int main(int argc, char *argv[]) {
-    //   pid_t fork_pid = fork();
-    //   if (fork_pid == 0) {
-    //     printf("Hello from the child!\n");
-    //   } else {
-    //     printf("Hello from the parent!\n");
-    //   }
-    //   return 0;
-    // }
-
     #include <unistd.h>
+    #include <stdio.h>
     int main(int argc, char *argv[]) {
-      execl("/bin/bash", "/yes/its/bash", "-c", "echo $0 && uptime", NULL);
-      
+      pid_t fork_pid = fork();
+      if (fork_pid == 0) {
+        printf("Hello from the child!\n");
+        int execl(const char *filename, const char *arg, ...);
+      } else {
+        printf("Hello from the parent!\n");
+      }
       return 0;
     }
+
+    // #include <unistd.h>
+    // int main(int argc, char *argv[]) {
+    //   execl("/bin/bash", "/yes/its/bash", "-c", "echo $0 && uptime", NULL);
+      
+    //   return 0;
+    // }
