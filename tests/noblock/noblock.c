afb34979aa923d13b898fdce43aac961438dddf5
mam1 <mam1@cornell.edu>
2021-02-12 11:22:16 -0500

    underdevelopment

--- tests/noblock/noblock.c
+++ tests/noblock/noblock.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
 
   const char        file[]="conwatcher";
   pid_t             fork_pid;
-  char *args[]={"file,NULL}; 
+  char              *args[]={file,NULL}; 
 
 
 
