121c235a47bd07c8cdbc61a9c683dde549f01b10
mam1 <mam1@cornell.edu>
2021-02-13 12:45:25 -0500

    underdevelopment

--- tests/conwatcher/conwatcher.c
+++ tests/conwatcher/conwatcher.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	lb_end = linebuff + _INPUT_BUFFER_SIZE;
 	while (1)
 	{
-		c = getch(stdin);						// read the keyboard
+		c = getchar();						// read the keyboard
 		printf("switching on <%c>\n", c);
 		switch (c)
 		{
