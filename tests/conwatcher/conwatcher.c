37738f5f6c89297dcadcc53ae02fb49efee8cfd5
mam1 <mam1@cornell.edu>
2021-02-14 10:17:38 -0500

    constructing

--- tests/conwatcher/conwatcher.c
+++ tests/conwatcher/conwatcher.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 	cbreak();
 	keypad(mainwin, TRUE);
 	clear();
-mvprintw(0, 0, "conwatcher active");/* Move to (y, x) then print string     */
+mvprintw(0, 0, "5555conwatcher active");/* Move to (y, x) then print string     */
 
 	mvprintw(0, 30, "enter a command > ");/* Move to (y, x) then print string     */
 	refresh();
