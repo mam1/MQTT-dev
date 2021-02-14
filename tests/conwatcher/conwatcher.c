5de6db13d5fd70216ddecd986899f9d68ff74ddf
mam1 <mam1@cornell.edu>
2021-02-14 11:36:38 -0500

    constructing

--- tests/conwatcher/conwatcher.c
+++ tests/conwatcher/conwatcher.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
 	/*  Switch of echoing and enable keypad (for arrow keys)  */
 	noecho();
-	cbreak();
+	// cbreak();
 	keypad(mainwin, TRUE);
 	clear();
 	mvprintw(0, 0, "conwatcher active");/* Move to (y, x) then print string     */
