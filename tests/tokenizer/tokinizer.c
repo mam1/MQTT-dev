5eddf2a7c1ce171012fd97fe2b9bc3d13e944e92
mam1 <mam1@cornell.edu>
2021-02-19 10:36:12 -0500

    more

--- tests/tokenizer/tokinizer.c
+++ tests/tokenizer/tokinizer.c
@@ -1,246 +1,126 @@
-include <unistd.h>
+
+#include "mysql.h"
 #include <stdio.h>
-#include "/home/mam1/Git/MQTT-dev/include/ipc.h"
-#include "/home/mam1/Git/MQTT-dev/include/typedefs.h"
-#include "/home/mam1/Git/MQTT-dev/include/shared.h"
+#include <stdlib.h>
+#include <unistd.h>   //sleep
+#include <string.h>
+#include <time.h>
 
+#define SERVER      "localhost"
+#define SOCKETT     "/run/mysqld/mysqld.sock"
+#define DATABASE    "tq"
+#define USER        "test-sql"
+#define PSWD        "test-sql"
 
+#define DAY 		2
+#define HOUR 		4
+#define MINUTE 		22
 
-int main(int argc, char *argv[])
+#define MINUTES_PER_HOUR 		60
+#define MINUTES_PER_DAY 		1440
+
+static void show_mysql_error(MYSQL *mysql)
 {
-	/* semaphores */
-	key_t           	skey = _SEM_KEY;
-	int             	semid;
+	printf("Error(%d) [%s] \"%s\"\n", mysql_errno(mysql),
+	       mysql_sqlstate(mysql),
+	       mysql_error(mysql));
+	exit(-1);
+}
 
-	/********** globals *******************************************************************/
-	_IPC_DAT       	ipc_dat, *ipc_ptr;              // ipc data
-	char           	ipc_file[] = {_IPC_FILE_NAME};  // name of ipc file
-	void           	*data;                      	// pointer to ipc data
-	int            	fd;                        		// file descriptor for ipc data file
-	int         	rtc;							// file descriptor for PCF8563 RTC
-	_tm         	tm;								// time date structure
-	key_t 			skey = _SEM_KEY;
-	int 			semid;
+int get_offset(int day, int hour, int minute) {
+	int 		off;
+	off = minute + (hour * MINUTES_PER_HOUR) + (day * MINUTES_PER_DAY);
+	return off;
+}
 
-	/* global memory mapped io variables */
-	unsigned short 	semval;
-	union semun {
-		int val;              						// used for SETVAL only
-		struct semid_ds *buf; 						// for IPC_STAT and IPC_SET
-		ushort *array;        						// used for GETALL and SETALL
-	};
-	union 			semun dummy;
-	SEMBUF sb = {0, -1, 0};  						// set to allocate resource
+void update_channel_state(void) {
+	time_t rawtime;
+	struct tm * timeinfo;
 
+	time ( &rawtime );
+	timeinfo = localtime ( &rawtime );
+	printf ( "Current local time and date: %s", asctime (timeinfo) );
+	printf("day of the week %i\n", timeinfo->tm_wday);
+	printf("get_offset = %i\n", get_offset(timeinfo->tm_wday, timeinfo->tm_hour, timeinfo->tm_min));
 
+	return;
+}
 
+int pushit(char * token)
+{
+	MYSQL               *conn;
+	MYSQL_RES           *result;
+	MYSQL_FIELD         *field;
+	MYSQL_ROW           row;
 
-	int 			cmd_buffer_push_index, cmd_buffer_pop_index;
-	char 			cmd_buffer[_CMD_BUFFER_DEPTH][_INPUT_BUFFER_SIZE + 1]; // array to hold multiple single arrays of characters
+	char 				buff[500];
 
-	uint8_t 		c;											// character typed on keyboard
-	int 			prompted = false;							// has a prompt been sent
-	int 			fd;											// file descriptor for ipc data file
-	char 			work_buffer[_INPUT_BUFFER_SIZE];			// contains the user input
-	char 			*work_buffer_ptr, *end_buff, *start_buff, *move_ptr;
-	char 			previous_work_buffer[_INPUT_BUFFER_SIZE];	// points to the  previous user input
-
-	char 			screen_buf[_SCREEN_BUFFER_SIZE], *cursor_ptr;
-	char 			*input_ptr, *hptr;
-
-	char 			ring_buffer[_CMD_BUFFER_DEPTH][_INPUT_BUFFER_SIZE];	// char array[NUMBER_STRINGS][STRING_MAX_SIZE];
-	int 			rb_in_idx, rb_out_idx;
-	int 			mv;
-	int  			escape;				// have we just processed an escape
-
-
-	/* set up file mapped shared memory for inter process communication */
-	ipc_sem_init();										// setup semaphores
-	semid = ipc_sem_id(skey);							// set semaphor id
-
-	/* set up shared memory */
-	ipc_sem_lock(semid, &sb);							// wait for a lock on shared memory
-	fd = ipc_open(ipc_file, ipc_size());      			// create/open ipc file
-	data = ipc_map(fd, ipc_size());           			// map file to memory
-	ipc_ptr = data; 									// overlay data with _IPC_DAT data structure
-	ipc_sem_free(semid, &sb);
-
-
-	/**********************************************************************************************/
-	printf("%s\n", "conwatcher active");
-
-	while (1)
+	/* get handles  */
+	conn = mysql_init(NULL);
+	if (conn == NULL)
 	{
-		c = getc(stdin);						// read the keyboard
-		switch (c)
-		{
-	/* NOCR */	case _NO_CHAR:
-			break;
-
-	/* ESC */ 	case _ESC:
-			c = getc(stdin);		// skip to next character
-			c = getc(stdin);		// skip to next character
-			switch (c)
-			{
-		/* up arrow */	case 'A':
-				if (rb_out_idx > 0)
-					rb_out_idx--;
-				else
-					rb_out_idx = rb_in_idx - 1;
-				if (rb_out_idx >= rb_in_idx)
-					rb_out_idx = 0;
-				arrow_reprompt();
-				continue;
-				break;
-		/* down arrow */case 'B':
-				rb_out_idx++;
-				if (rb_out_idx >= rb_in_idx)
-					rb_out_idx = 0;
-				arrow_reprompt();
-				continue;
-				break;
-
-		/* right arrow */case 'C':
-				if (input_ptr < work_buffer_ptr) {
-					input_ptr++;
-					printf("\033[1C");	// move cursor right
-				}
-				continue;
-				break;
-		/* left arrow */case 'D':
-				if (input_ptr > start_buff) {
-					input_ptr--;
-					printf("\033[1D");	// move cursor left
-				}
-				continue;
-				break;
-		/* ESC */		default:
-				escape = true;
-
-
-				while (pop_cmd_q(cmd_fsm_cb.token)); 						// empty command queue
-				memset(work_buffer, '\0', sizeof(work_buffer));				// clean out work buffer
-				memset(previous_work_buffer, '\0', sizeof(work_buffer));	// clean out previous command buffer
-				work_buffer_ptr = work_buffer;								// set pointer to start of buffer
-				input_ptr = work_buffer;									// set pointer to start of buffer
-				char_fsm_reset();											// initialize the character fsm
-				cmd_fsm_reset(&cmd_fsm_cb); 								// initialize the command processor fsm
-				char_state = 0;
-				prompted = false;											// force a prompt
-				printf("\n\rcommand processor reset\n\r");
-				strcpy(cmd_fsm_cb.prompt_buffer, "enter a command");
-				continue;
-				break;
-			}
-			// /* CR */	case _CR:
-
-			// 		if (work_buffer_ptr != start_buff) 						// skip null input lines
-			// 		{
-			// 			if (strcmp(work_buffer, previous_work_buffer) != 0)	// remove duplicates
-			// 			{
-			// 				strcpy(&ring_buffer[rb_in_idx++][0], work_buffer);
-			// 				if (rb_in_idx > _CMD_BUFFER_DEPTH - 1)
-			// 					rb_in_idx = 0;
-			// 				rb_out_idx = rb_in_idx;
-			// 				memset(previous_work_buffer, '\0', sizeof(work_buffer));
-			// 				strcpy(previous_work_buffer, work_buffer);
-			// 			}
-			// 		}
-			// 		printf("\n\r");						// move cursor to next line
-			// 		*work_buffer_ptr++ = _CR;			// load the CR into the work buffer
-			// 		*work_buffer_ptr++ = '\0';			// load the NULL into the work buffer
-			// 		work_buffer_ptr = work_buffer;		// reset pointer
-			// 		char_fsm_reset();					// reset char_fsm
-			// 		while (*work_buffer_ptr != '\0')	// send characters to char_fsm
-			// 		{
-			// 			char_fsm(char_type(*work_buffer_ptr), &char_state, work_buffer_ptr);
-			// 			work_buffer_ptr++;
-			// 		}
-
-			// 		work_buffer_ptr = work_buffer;		// reset pointer
-			// 		input_ptr = work_buffer_ptr;		// reset pointer
-			// 		memset(work_buffer, '\0', sizeof(work_buffer));
-			// 		memset(screen_buf, '\0', sizeof(screen_buf));
-			// 		memset(&ring_buffer[rb_in_idx][0], '\0', _INPUT_BUFFER_SIZE);
-
-			// 		break;
-			// /* DEL */	case _DEL:
-			// 		if (input_ptr == start_buff)
-			// 			break;
-
-			// 		if (input_ptr == work_buffer_ptr) {	// no arrow keys in play
-			// 			*work_buffer_ptr-- = '\0';
-			// 			*work_buffer_ptr = '\0';
-			// 			input_ptr = work_buffer_ptr;
-			// 			printf("\r");
-			// 			del_prompt(cmd_fsm_cb.state);		// display user prompt
-			// 			printf("%s", work_buffer);		// print work_buffer
-			// 			printf("\033[K");				// Erase to end of line
-			// 			prompted = true;
-			// 		}
-			// 		else
-			// 		{
-			// 			mv = work_buffer_ptr - input_ptr;
-			// 			input_ptr--;
-			// 			hptr = input_ptr;
-			// 			while (input_ptr < work_buffer_ptr)	// shift buffer left
-			// 			{
-			// 				*input_ptr = *(input_ptr + 1);
-			// 				input_ptr++;
-			// 			}
-			// 			input_ptr = hptr;
-			// 			*work_buffer_ptr-- = '\0';
-			// 			*work_buffer_ptr = '\0';
-
-			// 			printf("\r");
-			// 			printf("\033[K");	// Erase to end of line
-			// 			del_prompt(cmd_fsm_cb.state);
-			// 			printf("%s", work_buffer);
-			// 			while (mv > 0) {
-			// 				printf("\033[1D");	// move cursor left
-			// 				mv--;
-			// 			}
-			// 		}
-			// 		break;
-
-			// /* OTHER */ default:
-			// 		if (work_buffer_ptr <= end_buff)		// room to add character ?
-			// 		{
-			// 			if (input_ptr == work_buffer_ptr) 	// cursor is at the end of the input buffer
-			// 			{
-			// 				*work_buffer_ptr++ = c;
-			// 				input_ptr = work_buffer_ptr;
-			// 				printf("%c", c);
-			// 			}
-			// 			else 								// cursor is not at the end of the input buffer
-			// 			{
-			// 				move_ptr = work_buffer_ptr++;
-			// 				move_ptr++;
-			// 				*move_ptr-- = '\0';
-			// 				while (move_ptr > input_ptr)
-			// 				{
-			// 					*move_ptr = *(move_ptr - 1);
-			// 					move_ptr--;
-			// 				}
-			// 				*input_ptr++ = c;
-			// 				mv = work_buffer_ptr - input_ptr;
-			// 				printf("\r");
-			// 				printf("\033[K");	// Erase to end of line
-			// 				prompt(cmd_fsm_cb.state);
-			// 				printf("%s", work_buffer);
-
-			// 				while (mv > 0)
-			// 				{
-			// 					printf("\033[1D");	// move cursor left
-			// 					mv--;
-			// 				}
-			// 			}
-			// 		}
-		}
-		/* do suff while waiting or the keyboard */
+		printf("couldn't initialize conn: %s\n", mysql_error(conn));
+		exit(1);
 	}
 
-	/************************************************************************************************/
+	/* initailze client library */
+	if (mysql_library_init(argc, argv, NULL))
+	{
+		printf("couldn't initialize MySQL client library\n");
+		exit(1);
+	}
 
-	return 0;
+	/* connect to server */
+	if (mysql_real_connect(conn, SERVER, USER, PSWD, DATABASE, 0, SOCKETT, CLIENT_INTERACTIVE) == NULL)
+	{
+		printf("couldn't connect to database\n");
+		exit(1);
+	}
+
+	/* position curser at newest token */
+	if (mysql_query(conn, "SELECT * FROM TokenQ ORDER BY tokenID LIMIT 1;"))
+		show_mysql_error(conn);
+
+	/* insert newest token */
+	sprintf(buff, "INSERT INTO TokenQ(token) VALUES(%s);", *token);
+	if (mysql_query(conn, buff))
+		show_mysql_error(conn);
+
+	return 1;
+}
+
+char * popit(char * token)
+{
+	MYSQL               *conn;
+	MYSQL_RES           *result;
+	MYSQL_FIELD         *field;
+	MYSQL_ROW           row;
+
+	/* get handles  */
+	conn = mysql_init(NULL);
+	if (conn == NULL)
+	{
+		printf("couldn't initialize conn: %s\n", mysql_error(conn));
+		exit(1);
+	}
+
+	/* initailze client library */
+	if (mysql_library_init(argc, argv, NULL))
+	{
+		printf("couldn't initialize MySQL client library\n");
+		exit(1);
+	}
+
+	/* connect to server */
+	if (mysql_real_connect(conn, SERVER, USER, PSWD, DATABASE, 0, SOCKETT, CLIENT_INTERACTIVE) == NULL)
+	{
+		printf("couldn't connect to database\n");
+		exit(1);
+	}
+	return token;
+}
+int main(int argc, char* argv[]) {
+	printf("%s\n", "testing push");
+	pushit("from c");
+	printf("%s\n", "normal termination");
 }
