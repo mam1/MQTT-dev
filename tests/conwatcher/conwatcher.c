2435885816f8f82c271c7ab695052cc602140017
mam1 <mam1@cornell.edu>
2021-02-20 15:46:10 -0500

    more

--- tests/conwatcher/conwatcher.c
+++ tests/conwatcher/conwatcher.c
@@ -25,6 +25,123 @@ void disp(char *str)
 	return;
 }
 
+
+int Tpush(char * token)
+{
+	MYSQL               *conn;
+	char 				buff[500];
+
+	/* get connection handle  */
+	conn = mysql_init(NULL);
+	if (conn == NULL)
+	{
+		printf("couldn't initialize conn: %s\n", mysql_error(conn));
+		exit(1);
+	}
+
+	/* connect to server */
+	if (mysql_real_connect(conn, SERVER, USER, PSWD, DATABASE, 0, SOCKETT, CLIENT_INTERACTIVE) == NULL)
+	{
+		printf("couldn't connect to database\n");
+		exit(1);
+	}
+
+	/* insert newest token */
+	sprintf(buff, "INSERT INTO TokenQ(token) VALUES('%s');", token);
+	if (mysql_query(conn, buff))
+		show_mysql_error(conn);
+
+	mysql_close(conn);
+	return 1;
+}
+
+char * Tpop(char * token)
+{
+	MYSQL               *conn;
+	MYSQL_RES           *result;
+	MYSQL_FIELD         *field;
+	MYSQL_ROW           row;
+
+	char 				*cptr, *bptr;
+
+	char 				buff[500];
+	int 				i;
+
+	/* get handles  */
+	conn = mysql_init(NULL);
+	if (conn == NULL)
+	{
+		printf("couldn't initialize conn: %s\n", mysql_error(conn));
+		exit(1);
+	}
+	/* connect to server */
+	if (mysql_real_connect(conn, SERVER, USER, PSWD, DATABASE, 0, SOCKETT, CLIENT_INTERACTIVE) == NULL)
+	{
+		printf("couldn't connect to database\n");
+		exit(1);
+	}
+
+
+	/*get the oldest row */
+	if (mysql_query(conn, "SELECT * FROM TokenQ ORDER BY tokenID LIMIT 1;"))
+		show_mysql_error(conn);
+
+	result = mysql_store_result(conn);
+	printf("we now have %i  active columns\n", mysql_num_fields(result));
+	for (i = 0; i < (int)mysql_num_fields(result); i++)
+	{
+		mysql_field_seek(result, i);
+		field = mysql_fetch_field(result);
+		printf("column %i  %s\n", i, field->name);
+	}
+	printf("\n");
+
+	if ((row = mysql_fetch_row(result)) == NULL) return NULL;
+
+	cptr = row[1];
+	bptr = token;
+	while (*cptr != '\0')
+	{
+		printf("moving <%c>\n", *cptr);
+		*bptr++ = *cptr++;
+	}
+	bptr = '\0';
+
+	printf("token is <%s>\n", row[1]);
+	printf("tokenID is <%s>\n", row[0]);
+
+	sprintf(buff, "delete from TokenQ WHERE tokenID = '%s';", row[0]);
+	if (mysql_query(conn, buff))
+		show_mysql_error(conn);
+
+	return token;
+}
+int tokenizer(char *lbuf) {
+
+	char 			*tbuf_ptr, *lbuf_ptr;
+	char 			tbuf[500];
+
+	tbuf_ptr = tbuf;
+	lbuf_ptr = lbuf;
+
+	memset(tbuf, '\0', 500);
+
+	while (*lbuf_ptr != '\0') {
+		while (*lbuf_ptr == ' ') lbuf_ptr++;
+		while ((*lbuf_ptr != ' ') & (*lbuf_ptr != '\0')) *tbuf_ptr++ = *lbuf_ptr++;
+		*tbuf_ptr = '\0';
+		printf("lbut_ptr -> '%c'\n", *lbuf_ptr);
+		printf("tbuf <%s>\n", tbuf );
+
+		Tpush(tbuf);
+		memset(tbuf, '\0', 500);
+		tbuf_ptr = tbuf;
+
+	}
+	return 1;
+}
+
+
 int main(int argc, char *argv[])
 {
 	lb_in = linebuff;
@@ -54,7 +171,6 @@ int main(int argc, char *argv[])
 		// read the keyboard
 		if ( isprint(ch) && !(ch & KEY_CODE_YES))
 		{
-
 			/*  If a printable character  */
 			if (lb_ptr <= lb_end - 1)		// room to add character ?
 			{
@@ -99,6 +215,7 @@ int main(int argc, char *argv[])
 
 		/* CR */	case 0xa:
 				disp("got a CR");
+				tokenizer(linebuff);
 				memset(linebuff, '\0', sizeof(linebuff));
 				lb_in = linebuff;
 				lb_out = linebuff;
