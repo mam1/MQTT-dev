5d6c7fe513c548fc8665d9fc0281951c898a1858
mam1 <mam1@cornell.edu>
2021-02-20 15:58:17 -0500

    more

--- tests/initDB.sql
+++ tests/initDB.sql
@@ -44,41 +44,6 @@ CREATE TABLE Channels (
 CREATE TABLE TokenQ (
     tokenID INTEGER NOT NULL AUTO_INCREMENT,
     token varchar(100) NOT NULL,
+    type enum('command','unkown','string','int')
     PRIMARY KEY (tokenID)
 );
-
-DELIMITER $$ ;
-CREATE OR REPLACE PROCEDURE push (IN newtoken varchar(100))
-    BEGIN
-        #DECLARE newtokenID INTEGER;
-        SELECT tokenID FROM TokenQ ORDER BY tokenID DESC LIMIT 1;
-        INSERT INTO TokenQ(token) VALUES(newtoken);
-    END;
-DELIMITER ; $$
-
-
-DELIMITER $$ ;
-#CREATE OR REPLACE PROCEDURE pop (OUT token varchar(100))
-CREATE OR REPLACE PROCEDURE pop (OUT token varchar(100))
-    BEGIN
-        #select * from TokenQ;
-        #DECLARE toke varchar(100)default " "; 
-        SELECT token INTO @toke FROM TokenQ WHERE 1 = 1 ORDER BY tokenID LIMIT 1;
-        #SELECT * FROM TokenQ ORDER BY tokenID LIMIT 1;
-        SELECT @toke;
-        #SET toke = token;
-        #SET token = "hi there";
-        SELECT CONCAT("HELLO"," ",@toke);
-    END;
-DELIMITER ; $$
-
-CALL push("test1");
-CALL push("test2");
-CALL push("test333");
-CALL push("test4");
-CALL push("ttttt");
-
-
-
-
-
