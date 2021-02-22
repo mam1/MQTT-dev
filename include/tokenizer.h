6ba7d16fa66163e96387d5fd3958f723be03cb40
mam1 <mam1@cornell.edu>
2021-02-22 10:15:31 -0500

    more

--- include/tokenizer.h
+++ include/tokenizer.h
@@ -1,6 +1,11 @@
 #ifndef _TOKENIZER_H_
 #define _TOKENIZER_H_
 
+/* char_fsm.c version */
+#define _MAJOR_VERSION_tokenizer		0
+#define _MINOR_VERSION_tokenizer		0
+
+
 int Tpush(char * token);
 char * Tpop(char * token);
 int tokenizer(char *lbuf); 
--- lib_sources/tokinizer.c
+++ lib_sources/tokinizer.c
@@ -27,8 +27,47 @@ static void show_mysql_error(MYSQL *mysql)
 	exit(-1);
 }
 
+/* return character type  */
+int char_type(char c) {
+	switch (c) {
+	case _COMMA:
+	case _SPACE:
+	case _COLON:
+	case _SLASH:
+		return "delimiter";
+	case _QUOTE:
+		return "quote";
+	case _DEL:
+		return "number";
+	case _CR:
+		return "enter";
+	}
+	return 4;
+}
 
-int Tpush(char * token)
+/* return token type */
+char * (token_type)
+{
+
+
+}
+
+int isadelimiter(char * c)
+{
+	switch (c) {
+	case _COMMA:
+	case _SPACE:
+	case _COLON:
+	case _SLASH:
+		return 1;
+	default:
+		return -1;
+
+	}
+
+}
+
+int Tpush(char * token, char * string)
 {
 	MYSQL               *conn;
 	char 				buff[500];
@@ -49,7 +88,7 @@ int Tpush(char * token)
 	}
 
 	/* insert newest token */
-	sprintf(buff, "INSERT INTO TokenQ(token) VALUES('%s');", token);
+	sprintf(buff, "INSERT INTO TokenQ(token) VALUES('%s');", token, string);
 	if (mysql_query(conn, buff))
 		show_mysql_error(conn);
 
@@ -58,87 +97,54 @@ int Tpush(char * token)
 }
 
 char * Tpop(char * token)
-{
-	MYSQL               *conn;
-	MYSQL_RES           *result;
-	MYSQL_FIELD         *field;
-	MYSQL_ROW           row;
-
-	char 				*cptr, *bptr;
-
-	char 				buff[500];
-	int 				i;
-
-	/* get handles  */
-	conn = mysql_init(NULL);
-	if (conn == NULL)
-	{
-		printf("couldn't initialize conn: %s\n", mysql_error(conn));
-		exit(1);
-	}
-	/* connect to server */
-	if (mysql_real_connect(conn, SERVER, USER, PSWD, DATABASE, 0, SOCKETT, CLIENT_INTERACTIVE) == NULL)
-	{
-		printf("couldn't connect to database\n");
-		exit(1);
-	}
 
 
-	/*get the oldest row */
-	if (mysql_query(conn, "SELECT * FROM TokenQ ORDER BY tokenID LIMIT 1;"))
-		show_mysql_error(conn);
-
-	result = mysql_store_result(conn);
-	printf("we now have %i  active columns\n", mysql_num_fields(result));
-	for (i = 0; i < (int)mysql_num_fields(result); i++)
-	{
-		mysql_field_seek(result, i);
-		field = mysql_fetch_field(result);
-		printf("column %i  %s\n", i, field->name);
-	}
-	printf("\n");
-
-	if ((row = mysql_fetch_row(result)) == NULL) return NULL;
-
-	cptr = row[1];
-	bptr = token;
-	while (*cptr != '\0')
-	{
-		printf("moving <%c>\n", *cptr);
-		*bptr++ = *cptr++;
-	}
-	bptr = '\0';
-
-	printf("token is <%s>\n", row[1]);
-	printf("tokenID is <%s>\n", row[0]);
-
-	sprintf(buff, "delete from TokenQ WHERE tokenID = '%s';", row[0]);
-	if (mysql_query(conn, buff))
-		show_mysql_error(conn);
-
-	return token;
-}
 int tokenizer(char *lbuf) {
 
 	char 			*tbuf_ptr, *lbuf_ptr;
 	char 			tbuf[500];
+	char 			quote = 0;
 
 	tbuf_ptr = tbuf;
 	lbuf_ptr = lbuf;
 
 	memset(tbuf, '\0', 500);
 
-	while (*lbuf_ptr != '\0') {
-		while (*lbuf_ptr == ' ') lbuf_ptr++;
-		while ((*lbuf_ptr != ' ') & (*lbuf_ptr != '\0')) *tbuf_ptr++ = *lbuf_ptr++;
-		*tbuf_ptr = '\0';
-		printf("lbut_ptr -> '%c'\n", *lbuf_ptr);
-		printf("tbuf <%s>\n", tbuf );
+	while (*lbuf_ptr != '\0')
+	{	// loop until the input buffer is empty
 
-		Tpush(tbuf);
-		memset(tbuf, '\0', 500);
-		tbuf_ptr = tbuf;
+		/* NULL */
+		if (lbuf_ptr == lbuf)
+		{
+			cobtinue
+		}
 
+		/* QUOTE */
+		if (*lbuf_ptr == _QUOTE)
+		{
+			memset(tbuf, '\0', sizeof(tbuf));
+			tbuf_ptr = tbuf;
+			lbuf_ptr++;													// skip the quote
+			while ((*lbuf_ptr != _QUOTE) && (*lbuf_ptr != '\0')) 		// look for ending quote or end of buffer
+				*tbuf_ptr++ = *lbuf_ptr++;								// move char to temp buffer
+			*(++tbuf_ptr) = '\0';
+			Tpush(tbuf, "string");
+			memset(tbuf, '\0', sizeof(tbuf));
+			tbuf_ptr = tbuf;
+		}
+
+		if (isadelimiter(*lbuf_ptr))
+		{
+			tbuf_ptr++ = '\0';
+			Tpush(tbuf, token_type(tbuf));
+			memset(tbuf, '\0', sizeof(tbuf));
+			tbuf_ptr = tbuf;
+			// while ((!isadelimiter(*lbuf_ptr)) && (*lbuf_ptr != '\0')) 		// look for delimiter or end of buffer
+			// 	*tbuf_ptr++ = *lbuf_ptr++;
+		}
+
+		*tbuf_ptr++ = *lbuf_ptr++;
 	}
-	return 1;
+	*tbuf_ptr++ = *lbuf_ptr++;
+	Tpush(tbuf, token_type(tbuf))
 }
