62e0ffbab593ad8c9ecfced2170295f417b7b882
mam1 <mam1@cornell.edu>
2021-02-22 11:51:00 -0500

    more

--- lib_sources/tokinizer.c
+++ lib_sources/tokinizer.c
@@ -2,7 +2,7 @@
 #include "/usr/include/mariadb/mysql.h"
 #include "/home/mam1/Git/MQTT-dev/include/typedefs.h"
 #include "/home/mam1/Git/MQTT-dev/include/shared.h"
-
+#include "/home/mam1/Git/MQTT-dev/include/tokenizer.h"
 
 #include <stdio.h>
 #include <stdlib.h>
