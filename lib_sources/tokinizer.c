cbbe83a455f1e2d87df70d3c51667267e90d96a1
mam1 <mam1@cornell.edu>
2021-03-01 15:56:22 -0500

    more

--- lib_sources/tokinizer.c
+++ lib_sources/tokinizer.c
@@ -98,9 +98,9 @@ printf("\n\n\n*********************** token <%s> *******\n\n\n", token->token);
 
 
 	memset(buff, '\0', sizeof(buff));
-	strcpy(buff, "SELECT * FROM KeyWords WHERE keyword = ");
+	strcpy(buff, "SELECT * FROM KeyWords WHERE keyword = '");
 	strcat(buff, token->token);
-	strcat(buff, ";");
+	strcat(buff, "';");
 	if (mysql_query(conn, buff))
 		show_mysql_error(conn);
 
