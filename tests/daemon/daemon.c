7c7c12d8cf2d672ff98d44b2bd56158d244b6489
mam1 <mam1@cornell.edu>
2021-03-07 11:59:48 -0500

    more

--- tests/daemon/daemon.c
+++ tests/daemon/daemon.c
@@ -33,7 +33,7 @@
 #include "/usr/include/mariadb/mysql.h"
 #include "/home/mam1/Git/MQTT-dev/include/typedefs.h"
 #include "/home/mam1/Git/MQTT-dev/include/shared.h"
-// #include "/home/mam1/Git/MQTT-dev/include/tokenizer.h"
+#include "/home/mam1/Git/MQTT-dev/include/tokenizer.h"
 
 
 
