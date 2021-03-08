5e4f3fd55f734b0b5de93a35b74bad971f6d625a
mam1 <mam1@cornell.edu>
2021-03-08 13:19:50 -0500

    more

--- include/typedefs.h
+++ include/typedefs.h
@@ -5,7 +5,7 @@
 
 
 /* buffer for semaphores */
-typedef struct sembuf {
+typedef struct {
 	unsigned short int sem_num; // semaphore #: 0 = first
 	short int sem_op; // semaphore operation
 	short int sem_flg; // operation flags
