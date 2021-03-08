fe12f05b25852a40d342c59d6d25bbf2c68f861c
mam1 <mam1@cornell.edu>
2021-03-08 14:36:17 -0500

    more

--- tests/conwatcher/conwatcher.c
+++ tests/conwatcher/conwatcher.c
@@ -5,9 +5,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <curses.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+
 
 /* local includes */
-#include "/home/mam1/Git/MQTT-dev/include/ipc.h"
+// #include "/home/mam1/Git/MQTT-dev/include/ipc.h"
 #include "/home/mam1/Git/MQTT-dev/include/typedefs.h"
 #include "/home/mam1/Git/MQTT-dev/include/shared.h"
 #include "/home/mam1/Git/MQTT-dev/include/tokenizer.h"
