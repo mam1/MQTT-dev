5e34ab2e41e5e70ab55cb83e0699bad004dfd065
mam1 <mam1@cornell.edu>
2021-03-08 14:46:37 -0500

    more

--- tests/toker/toker.c
+++ tests/toker/toker.c
@@ -1,36 +1,36 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <string.h>
+// #include <sys/types.h>
+// #include <sys/ipc.h>
+// #include <sys/sem.h>
 
-#define KEY 0x1111
+// #define KEY 0x1111
 
-union semun {
-	int val;
-	struct semid_ds *buf;
-	unsigned short  *array;
-};
+// union semun {
+// 	int val;
+// 	struct semid_ds *buf;
+// 	unsigned short  *array;
+// };
 
-struct sembuf p = { 0, -1, SEM_UNDO};
-struct sembuf v = { 0, +1, SEM_UNDO};
+// struct sembuf p = { 0, -1, SEM_UNDO};
+// struct sembuf v = { 0, +1, SEM_UNDO};
 
-int main()
-{
-	int id = semget(KEY, 1, 0666 | IPC_CREAT);
-	if (id < 0)
-	{
-		perror("semget"); exit(11);
-	}
-	union semun u;
-	u.val = 1;
-	if (semctl(id, 0, SETVAL, u) < 0)
-	{
-		perror("semctl"); exit(12);
-	}
+// int main()
+// {
+// 	int id = semget(KEY, 1, 0666 | IPC_CREAT);
+// 	if (id < 0)
+// 	{
+// 		perror("semget"); exit(11);
+// 	}
+// 	union semun u;
+// 	u.val = 1;
+// 	if (semctl(id, 0, SETVAL, u) < 0)
+// 	{
+// 		perror("semctl"); exit(12);
+// 	}
 
 
 
