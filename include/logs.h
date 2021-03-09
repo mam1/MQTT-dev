f6c040adf37e950f622d2cd3a824413ae420932a
mam1 <mam1@cornell.edu>
2021-03-09 15:54:19 -0500

    more

---
+++ include/logs.h
@@ -0,0 +1,5 @@
+#ifndef LOGS_H_
+#define LOGS_H_
+void logit(char * fname, char *source, char *mess);
+
+#endif
---
+++ lib_sources/logs.c
@@ -0,0 +1,60 @@
+
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <unistd.h>		//sleep
+#include <stdint.h>		//uint_8, uint_16, uint_32, etc.
+#include <ctype.h> 		//isalnum, tolower
+#include <stdlib.h>
+#include <fcntl.h>
+#include <time.h>
+
+#include "/home/mam1/Git/MQTT-dev/include/typedefs.h"
+#include "/home/mam1/Git/MQTT-dev/include/shared.h"
+#include "/home/mam1/Git/MQTT-dev/include/ipc.h"
+
+
+
+/* write an entry to the daemon log file */
+void logit(char * fname, char *source, char *mess) {
+	FILE 				*dlog, *slog;
+	char 				* time_now, *tnptr;
+	time_t 				t;
+
+	t = time(NULL);
+	time_now = ctime(&t);
+	tnptr = time_now;
+	while (*tnptr != _CR) tnptr++;
+	*tnptr = '\0';
+
+
+	/* Open process log file */
+	dlog = fopen(fname, "a");
+	if (dlog == NULL) {
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(dlog, "%s - %s\n", time_now, mess);
+	fclose(dlog);
+
+
+	/* Open system log file */
+	slog = fopen(_SYSTEM_LOG, "a");
+	if (slog == NULL) {
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(slog, "%s - %s\n", time_now, mess);
+	fclose(slog);
+
+
+
+	return;
+}
