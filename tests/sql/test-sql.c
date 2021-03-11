52da68a0572cf1a52f679b8b5adadc72339bdea4
mam1 <mam1@cornell.edu>
2021-03-11 12:12:56 -0500

    more

--- tests/sql/test-sql.c
+++ tests/sql/test-sql.c
@@ -30,7 +30,8 @@
 
 #define STRING_SIZE 50
 
-#define SELECT_SAMPLE "SELECT tokenID, token, type, value FROM TokenQ WHERE !SLEEP(1)"
+#define SELECT_SAMPLE "SELECT tokenID, token, type, value FROM TokenQ"
+// #define SELECT_SAMPLE "SELECT tokenID, token, type, value FROM TokenQ WHERE !SLEEP(1)"
 
 int main(int argc, char **argv) {
   MYSQL_STMT    *stmt;
