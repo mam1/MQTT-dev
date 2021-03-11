1b64ee8405aaf5fab09ae5fd28ec7be8fb55bec4
mam1 <mam1@cornell.edu>
2021-03-11 15:18:49 -0500

    more

---
+++ tests/ttpop/ttpop.c
@@ -0,0 +1,39 @@
+#include <unistd.h>		//sleep
+#include <stdint.h>		//uint_8, uint_16, uint_32, etc.
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <curses.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <time.h>
+
+
+/* local includes */
+#include "/home/mam1/Git/MQTT-dev/include/ipc.h"
+#include "/home/mam1/Git/MQTT-dev/include/typedefs.h"
+#include "/home/mam1/Git/MQTT-dev/include/shared.h"
+#include "/home/mam1/Git/MQTT-dev/include/tokenizer.h"
+#include "/home/mam1/Git/MQTT-dev/include/logs.h"
+
+/* database connection */
+#include "/usr/include/mariadb/mysql.h"
+#define SERVER      "localhost"
+#define SOCKETT     "/run/mysqld/mysqld.sock"
+#define DATABASE    "tokenTest"
+#define USER        "test-sql"
+#define PSWD        "test-sql"
+
+int main(void)
+{
+	_TOKEN 				t;
+
+	Tpop(&t);
+
+	printf("token dump:  <%s>  <%s>  <%i>\n", t.token, t.type, t.value);
+	return 0;
+}
