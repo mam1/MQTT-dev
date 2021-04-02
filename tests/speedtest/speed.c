b8dc0627f6bd17825fba558f41ef3cb553328ddb
mam1 <mam1@cornell.edu>
2021-04-02 13:42:17 -0400

    initial load

--- tests/speedtest/speed.c
+++ tests/speedtest/speed.c
@@ -31,7 +31,7 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		printf("field %i value <%s>\n", i, value[i][0]);
+		printf("field %i value <%s>\n", i, &value[i][0]);
 	}
 
 	printf("%s\n", "normal termination");
